ec64a1f24d71ecfa4eb3211b0062a89ef7df3b10
Daniel <wither.tweets@gmail.com>
2022-10-01 10:42:07 +0200

    Implemented smooth camera damping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Player Properties")] 
     public float m_Speed = 0.5f;
+
+    public Transform m_PlayerCamera;
+
+    private Vector3 m_CurrentVelocity;
     void Start()
     {
         
@@ -17,8 +21,14 @@ public class PlayerController : MonoBehaviour
         float xAxis = Input.GetAxis("Horizontal");
         float yAxis = Input.GetAxis("Vertical");
 
-        Vector3 currentTransform = this.transform.position;
-        currentTransform += new Vector3(xAxis * Time.deltaTime * m_Speed, yAxis * Time.deltaTime * m_Speed, 0);
+        Vector2 currentTransform = this.transform.position;
+        Vector2 targetPosition = new Vector2(xAxis * Time.deltaTime * m_Speed, yAxis * Time.deltaTime * m_Speed);
+
+        currentTransform += targetPosition;
         this.transform.position = currentTransform;
+
+        Vector3 cameraTargetPos = new Vector3(currentTransform.x, currentTransform.y, -10);
+        m_PlayerCamera.position =
+            Vector3.SmoothDamp(m_PlayerCamera.position, cameraTargetPos, ref m_CurrentVelocity, 0.5f, 5f);
     }
 }
